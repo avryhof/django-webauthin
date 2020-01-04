6c2c0aa283de29158a92e9b771ac86b4dfd7ed70
Stavros Korokithakis <hi@stavros.io>
2020-01-04 13:44:42 +0200

    fix: Make the admin list view more presentable

--- webauthin/admin.py
+++ webauthin/admin.py
@@ -5,5 +5,5 @@ from .models import AuthData
 
 @admin.register(AuthData)
 class AuthDataAdmin(admin.ModelAdmin):
-    list_display = ["user", "credential_id", "created_on", "last_used_on"]
-    search_fields = ["credential_id"]
+    list_display = ["user", "name", "created_on", "last_used_on"]
+    search_fields = ["name", "credential_id"]
